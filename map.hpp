1cba5fe6e7413b86d059f8c6462b90d822e42cc7
adidion19 <didionantoine@gmail.com>
2022-03-16 17:16:23 +0100

    je reflechis encore a comment faire le RBtree

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: adidion <adidion@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 14:01:20 by adidion           #+#    #+#             */
-/*   Updated: 2022/03/15 14:13:21 by adidion          ###   ########.fr       */
+/*   Updated: 2022/03/16 17:10:42 by adidion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,91 @@
 #include <memory>
 #include <iterator>
 #include <exception>
+# include "pair.hpp"
 
 namespace ft
 {
 	template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<ft::pair<const Key,T> > >
-	class map;
+	class map
+	{
+		public:
+			typedef Key key_type;
+			typedef T mapped_type;
+			typedef ft::pair<const key_type, mapped_type> value_type;
+			typedef Compare key_compare;
+			//typedef 
+			typedef Alloc allocator_type;
+			typedef typename allocator_type::reference		reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename Alloc::pointer					pointer;
+			typedef typename Alloc::const_pointer			const_pointer;
+			typedef std::ptrdiff_t	difference_type;
+			typedef std::size_t size_type;
+		// CONSTRUCTEURS
+			explicit map (const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type());
+			template <class InputIterator>
+			map (InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type());
+			map (const map& x);
+		// DESTRUCTEUR
+			~map();
+		// OPERATEUR =
+			map& operator= (const map& x);
+		// ITERATEURS
+			iterator begin();
+			const_iterator begin() const;
+			iterator end();
+			const_iterator end() const;
+			reverse_iterator rbegin();
+			const_reverse_iterator rbegin() const;
+			reverse_iterator rend();
+			const_reverse_iterator rend() const;
+		//CAPACITY
+			bool empty() const;
+			size_type size() const;
+			size_type max_size() const;
+		//ELEMENT ACCESS
+			mapped_type& operator[] (const key_type& k);
+		// MODIFIERS
+			pair<iterator,bool>
+			insert (const value_type& val);
+			iterator insert (iterator position, const value_type& val);
+			template <class InputIterator>
+			void insert (InputIterator first, InputIterator last);
+			void erase (iterator position);
+			size_type erase (const key_type& k);
+			void erase (iterator first, iterator last);
+			void swap (map& x);
+			void clear();
+		// OBSERVERS
+			key_compare key_comp() const;
+			value_compare value_comp() const;
+		// OPERATIONS
+			iterator find (const key_type& k);
+			const_iterator find (const key_type& k) const;
+			size_type count (const key_type& k) const;
+			iterator lower_bound (const key_type& k);
+			const_iterator lower_bound (const key_type& k) const;
+			iterator upper_bound (const key_type& k);
+			const_iterator upper_bound (const key_type& k) const;
+			pair<const_iterator,const_iterator> equal_range (const key_type& k) const;
+			pair<iterator,iterator> equal_range (const key_type& k);
+		// ALLOCATOR
+			allocator_type get_allocator() const;
+	};
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator==( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs );
+	template< class Key, class T, class Compare, class Alloc > 
+	bool operator!=( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs );
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator<( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs );
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator<=( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs );
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator>( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs );
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator>=( const ft::map<Key,T,Compare,Alloc>& lhs, const ft::map<Key,T,Compare,Alloc>& rhs );
+	template< class Key, class T, class Compare, class Alloc >
+	void swap( ft::map<Key,T,Compare,Alloc>& lhs, ft::map<Key,T,Compare,Alloc>& rhs );
 }
 
 #endif
---
+++ pair.hpp
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pair.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adidion <adidion@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 14:20:48 by adidion           #+#    #+#             */
+/*   Updated: 2022/03/15 15:22:31 by adidion          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PAIR_H
+# define PAIR_H
+
+#include <iostream>
+#include <memory>
+#include <iterator>
+#include <exception>
+
+namespace ft
+{
+	template < class T1, class T2 >
+	struct pair
+	{ // 1 2 4
+		public:
+			typedef T1 first_type;
+			typedef T2 second_type;
+			first_type	first;
+			second_type	second;
+			pair(): first(first_type()), second(second_type())
+			{
+				return ;
+			}
+			pair( const T1& x, const T2& y ): first(x), second(y)
+			{
+				return ;
+			}
+			template< class U1, class U2 >
+			pair( const pair<U1, U2>& p )
+			{
+				first = p.first;
+				second = p.second;
+				return ;
+			}
+			pair& operator=( const pair& other )
+			{
+				first = ref.first;
+				second = ref.second;
+				return (*this);
+			}
+	};
+
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{
+		return (lhs.first == rhs.first && lhs.second == rhs.second);
+	}
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{
+		return (!(lhs == rhs));
+	}
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{
+		return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
+	}
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{
+		return (!(rhs < lhs));
+	}
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{
+		return (rhs < lhs);
+	}
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{
+		return (!(lhs<rhs));
+	}
+
+	template <class T1, class T2>
+	pair<T1,T2> make_pair (T1 x, T2 y)
+	{
+		return (pair<T1, T2>(x, y));
+	}
+}
+
+#endif
